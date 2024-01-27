1219b1f55ca77399ad212648d9b8fe853e4038c3
rhy3h <rhy3htw@gmail.com>
2024-01-27 16:04:46 +0800

    refactor: Format

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import "@/assets/styles/main.scss";
+import '@/assets/styles/main.scss';
 
 import {
-  WebGLRenderer,
-  PerspectiveCamera,
-  Scene,
-  BoxGeometry,
-  Mesh,
-  MeshBasicMaterial,
-  Color,
-} from "three";
+    WebGLRenderer,
+    PerspectiveCamera,
+    Scene,
+    BoxGeometry,
+    Mesh,
+    MeshBasicMaterial,
+    Color,
+} from 'three';
 
 let scene: Scene, camera: PerspectiveCamera, renderer: WebGLRenderer;
 let cube: Mesh;
@@ -16,34 +16,38 @@ let cube: Mesh;
 init();
 
 function init() {
-  scene = new Scene();
-  scene.background = new Color(0xcce0ff);
 
-  camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  camera.position.z = 5;
+    scene = new Scene();
+    scene.background = new Color(0xcce0ff);
 
-  renderer = new WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+    camera = new PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    camera.position.z = 5;
 
-  const geometry = new BoxGeometry(1, 1, 1);
-  const material = new MeshBasicMaterial({ color: 0x000000 });
-  cube = new Mesh(geometry, material);
-  scene.add(cube);
+    renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-  animate();
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({ color: 0x000000 });
+    cube = new Mesh(geometry, material);
+    scene.add(cube);
+
+    animate();
+
 }
 
 function animate() {
-  requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+    requestAnimationFrame(animate);
 
-  renderer.render(scene, camera);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,63 +1,63 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import TerserPlugin from "terser-webpack-plugin";
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
 
 module.exports = {
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "three": path.resolve(__dirname, "node_modules/three")
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+        ],
     },
-    extensions: [".ts", ".js"],
-  },
-  output: {
-    filename: "[name].[chunkhash].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            'three': path.resolve(__dirname, 'node_modules/three')
         },
-      }),
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: '[name].[chunkhash].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: true,
+                    },
+                },
+            }),
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.ico',
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/[name].[chunkhash].bundle.css',
+        }),
+        new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      favicon: "./public/favicon.ico",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "./css/[name].[chunkhash].bundle.css",
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    compress: true,
-    port: 4000,
-    open: true,
-  },
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 4000,
+        open: true,
+    },
 };
