43ba4a898bf2ebd33a731e917426952de7821e5a
rhy3h <rhy3htw@gmail.com>
2024-01-27 19:48:30 +0800

    feat: Adjust to NodeMaterial

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import '@/assets/styles/main.scss';
 
+import { MeshBasicNodeMaterial, color } from 'three/examples/jsm/nodes/Nodes';
+import { nodeFrame } from 'three/examples/jsm/renderers/webgl-legacy/nodes/WebGLNodes';
+
 import {
     WebGLRenderer,
     PerspectiveCamera,
     Scene,
     BoxGeometry,
     Mesh,
-    MeshBasicMaterial,
     Color,
 } from 'three';
 
@@ -33,7 +35,9 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     const geometry = new BoxGeometry(1, 1, 1);
-    const material = new MeshBasicMaterial({ color: 0x000000 });
+    const material = new MeshBasicNodeMaterial();
+    material.colorNode = color( 0x000000 );
+
     cube = new Mesh(geometry, material);
     scene.add(cube);
 
@@ -44,6 +48,8 @@ function init() {
 function animate() {
 
     requestAnimationFrame(animate);
+
+    nodeFrame.update();
 
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
